0a814a875cda6199b260fdb7575e253ddec58355
Podenko Aleksandr <sanick1992@gmail.com>
2015-04-10 19:39:04 +0300

    last v.

--- header.php
+++ header.php
@@ -37,10 +37,6 @@
                             'menu_id' => '',
                             'echo' => true,
                             'fallback_cb' => 'wp_page_menu',
-                            'before' => '',
-                            'after' => '',
-                            'link_before' => '',
-                            'link_after' => '',
                             'items_wrap' => '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
                             'depth' => 0,
                             'walker' => ''
